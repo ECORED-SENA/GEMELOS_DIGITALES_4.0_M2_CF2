e88d81ec28d135ae62fc3ff6c2f212783ffe73a2
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2021-11-02 15:38:54 -0500

    Correcciones 7
    Se actualiza titulo actividad didáctica.

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -10,7 +10,7 @@
             img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Actividad didáctica
+          h2 Relacionar correctamente la secuencia de pasos
         p.mb-4 Verificación de saberes adquiridos en el modelado de superficies 3D en forma libre.
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
